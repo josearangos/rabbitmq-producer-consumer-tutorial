aa597d012f4c59ef776211acac617bbad3ea1820
josearangos <jose.arangos@udea.edu.co>
2020-11-01 12:29:56 -0500

    producer and consumer files

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # rabbitmq-producer-consumer-tutorial
+
+
+This code is based in youtube tutorial
+
+https://www.youtube.com/watch?v=Wiw7oOgBjFs&list=PLL2hlSFBmWwy8lhnj11FVJldKsZm66oq1&index=3&ab_channel=soumilshah1995
+
+Thanks soumilshah1995 for share this tutorial series
+
+Docker rabbitmq
+
+docker run --rm -it --hostname rabbit-container -p 15672:15672 -p 5672:5672 rabbitmq:3-management
+
+
+
+The port 5672 is used for the RabbitMQ client connections, and the port 15672 is for the RabbitMQ management website
+http://localhost:15672
+
+
+username and password of guest / guest
---
+++ producer.py
@@ -0,0 +1,38 @@
+from pika.spec import Queue
+
+
+try:
+    import pika
+
+except Exception as e:
+    print("Sone Modules are missings {}".format_map(e))
+
+"""
+class MetaClass(type):
+
+    _instance ={}
+
+    def __call__(cls, *args, **kwargs):
+
+        # Singelton Design Pattern  
+
+        if cls not in cls._instance:
+            cls._instance[cls] = super(MetaClass, cls).__call__(*args, **kwargs)
+            return cls._instance[cls]
+
+
+"""
+class RabbitMq(object):
+    def __init__(self,queue='hello'):
+        self._connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        self._channel = self._connection.channel()
+        self.queue=queue
+        self._channel.queue_declare(queue=self.queue)
+    def publish(self,payload = {}):
+        self._channel.basic_publish(exchange='', routing_key='hello',body=str(payload))
+        print("Published ...")
+        self._connection.close()
+
+if __name__ == "__main__":
+    server = RabbitMq(queue='hello')
+    server.publish(payload={"Data":"Hi"})
