590dce9f13bdf862c47d8a31910f9030e99bfeec
josearangos <jose.arangos@udea.edu.co>
2020-11-01 14:37:31 -0500

    good practices

--- consumer.py
+++ consumer.py
@@ -1,28 +1,55 @@
-from pika import connection
-from pika import channel
-
-
 try:
     import pika
+    import ast
 
 except Exception as e:
     print("Sone Modules are missings {}".format_map(e))
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost')
-)
+class MetaClass(type):
 
-channel = connection.channel()
-channel.queue_declare(queue='hello')
+    _instance ={}
+
+    def __call__(cls, *args, **kwargs):
+        # Singelton Design Pattern  
+        if cls not in cls._instance:
+            cls._instance[cls] = super(MetaClass, cls).__call__(*args, **kwargs)
+            return cls._instance[cls]
+
+class RabbitMqServerConfigure(metaclass=MetaClass):
+
+    def __init__(self, host='localhost',queue='hello'):
+        """ Server initialization """
+        self.host = host
+        self.queue = queue
 
 
+class rabbitmqServer():
+    def __init__(self,server):
+        """
+        :param server : Object of class RabbitMqServerConfigure
 
-def callback(ch,method,properties, body):
-    print(" [x] Received %r "%body)
+        """
+        self.server = server
+        self._connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.server.host))
+        self._channel = self._connection.channel()
+        self._tem = self._channel.queue_declare(queue=self.server.queue)
+        print("Server started waiting for Messages ")
+    
+    @staticmethod
+    def callback(ch, method, properties, body):
+        payload = body.decode("utf-8")
+        payload = ast.literal_eval(payload)
+        print("Data Received : {}".format(payload))
 
-channel.basic_consume(
-    queue='hello',on_message_callback=callback, auto_ack=False
-)
 
-print(' [*] Waiting for messages. To exit press CTRL +C')
-channel.start_consuming()
+    def startServer(self):
+        self._channel.basic_consume(
+            queue=self.server.queue,
+            on_message_callback=rabbitmqServer.callback,
+            auto_ack=False)
+        self._channel.start_consuming()
+
+if __name__ == "__main__":
+    serverConfigure = RabbitMqServerConfigure(host='localhost',queue='hello')
+    server = rabbitmqServer(server = serverConfigure)
+    server.startServer()
--- producer.py
+++ producer.py
@@ -1,38 +1,63 @@
-from pika.spec import Queue
-
-
 try:
     import pika
 
 except Exception as e:
     print("Sone Modules are missings {}".format_map(e))
 
-"""
 class MetaClass(type):
 
     _instance ={}
 
     def __call__(cls, *args, **kwargs):
-
         # Singelton Design Pattern  
-
         if cls not in cls._instance:
             cls._instance[cls] = super(MetaClass, cls).__call__(*args, **kwargs)
             return cls._instance[cls]
 
-
-"""
-class RabbitMq(object):
-    def __init__(self,queue='hello'):
-        self._connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-        self._channel = self._connection.channel()
+class RabbitmqConfigure(metaclass = MetaClass):
+    def __init__(self, queue='hello',host='localhost', routingKey = 'hello',exchange=''):
         self.queue=queue
-        self._channel.queue_declare(queue=self.queue)
-    def publish(self,payload = {}):
-        self._channel.basic_publish(exchange='', routing_key='hello',body=str(payload))
-        print("Published ...")
+        self.host=host
+        self.routingKey = routingKey
+        self.exchange = exchange
+
+class RabbitMq():
+
+    __slots__ = ["server","_channel","_connection"]
+
+    def __init__(self,server):
+        """
+        :param server: Object of class RabbitmqConfigure
+        """
+        self.server = server
+        self._connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.server.host))
+        self._channel = self._connection.channel()
+        self._channel.queue_declare(queue=self.server.queue)
+
+    def __enter__(self):
+        print("__enter__")
+        return self
+
+    def __exit__(self,exc_type,exc_val,exc_tb):
+        print("__exit__")
         self._connection.close()
 
+
+
+    def publish(self,payload = {}):
+        """
+        :param payload: JSON payload 
+        :return: None
+
+        """
+        self._channel.basic_publish(exchange=self.server.exchange, routing_key=self.server.routingKey,body=str(payload))
+        print("Published Message: {}".format(payload))
+        
+
 if __name__ == "__main__":
-    server = RabbitMq(queue='hello')
-    server.publish(payload={"Data":"Hi"})
+
+    server = RabbitmqConfigure(queue='hello',host='localhost', routingKey = 'hello',exchange='')
+    
+    with RabbitMq(server) as rabbitmq:
+        rabbitmq.publish(payload={"Data":7475})
+ 
